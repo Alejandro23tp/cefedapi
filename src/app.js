6cfa8aff3232fd747da4c5a47ab8c8144ef0a6a0
Alejandro <alejandrolias84@gmail.com>
2024-12-22 02:26:49 -0500

    post eventos

--- src/app.js
+++ src/app.js
@@ -1,36 +1,39 @@
-import express from "express"; // Importa Express
-import multer from "multer"; // Importa Multer
-import path from "path"; // Para manejar rutas
-import cors from "cors"; // Importa CORS
-import media_routes from "./routes/media_routes.js";
-import events_routes from "./routes/events_routes.js";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import mediaRoutes from "./routes/media_routes.js";
+import eventsRoutes from "./routes/events_routes.js";  // Asegúrate de importar correctamente las rutas de eventos
 
 const app = express();
-app.use(cors({ origin: "https://cefedapi-arpx.onrender.com" }));
-app.use(express.json()); //interprete los objetos enviados como json
 
-// Servir archivos estáticos de las carpetas de imágenes y videos
+// Configuración de CORS para permitir múltiples orígenes
+const corsOptions = {
+  origin: ["http://localhost:4200", "https://cefedapi-arpx.onrender.com"],  // Añadir ambos orígenes aquí
+  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"],  // Encabezados permitidos
+};
+
+app.use(cors(corsOptions));  // Aplicar configuración de CORS
+
+// Para manejar los datos JSON
+app.use(express.json());
+
+// Servir archivos estáticos de imágenes y videos
 app.use("/uploads/images", express.static(path.resolve("uploads/images")));
 app.use("/uploads/videos", express.static(path.resolve("uploads/videos")));
 
 // Rutas de medios (imágenes y videos)
-app.use("/api", media_routes);
+app.use("/api", mediaRoutes);
+app.use("/api", eventsRoutes); // Rutas de eventos
 
 // Ruta principal
 app.get("/", (req, res) => {
   res.send("Hola desde el servidor");
 });
 
-
-app.use("/api", events_routes); // Ajusta esta línea si es necesario
-
-// Crear el directorio para las imágenes de eventos
-app.use("/uploads/images/events", express.static(path.resolve("uploads/images/events")));
-
 // Middleware para manejar rutas no encontradas
 app.use((req, res, next) => {
   res.status(404).json({ message: "Página no encontrada" });
-  //presentar paginas en caso de que no exista la ruta
 });
 
 export default app;
