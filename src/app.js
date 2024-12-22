cf1e4d795551e5e538f39c7054cb15e2b4c49084
Alejandro <alejandrolias84@gmail.com>
2024-12-22 01:43:11 -0500

    eventos api

--- src/app.js
+++ src/app.js
@@ -3,10 +3,19 @@ import multer from "multer"; // Importa Multer
 import path from "path"; // Para manejar rutas
 import cors from "cors"; // Importa CORS
 import media_routes from "./routes/media_routes.js";
+import events_routes from "./routes/events_routes.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:4200" }));
-app.use(express.json()); //interprete los objetos enviados como json
+
+// Permitir CORS para todos los orígenes y métodos
+app.use(cors({
+  origin: "*", // Permitir todos los orígenes
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"], // Cabeceras permitidas
+  credentials: true // Permitir cookies si es necesario
+}));
+
+app.use(express.json()); // Interpreta los objetos enviados como JSON
 
 // Servir archivos estáticos de las carpetas de imágenes y videos
 app.use("/uploads/images", express.static(path.resolve("uploads/images")));
@@ -19,6 +28,12 @@ app.use("/api", media_routes);
 app.get("/", (req, res) => {
   res.send("Hola desde el servidor");
 });
+
+// Rutas de eventos
+app.use("/api", events_routes);
+
+// Crear el directorio para las imágenes de eventos
+app.use("/uploads/images/events", express.static(path.resolve("uploads/images/events")));
 
 // Middleware para manejar rutas no encontradas
 app.use((req, res, next) => {
